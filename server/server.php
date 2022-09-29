4458779ee6a781e9f711ca050c056a6b2d68d243
Andrian Elemento <andrianelemento41@gmail.com>
2022-09-29 17:23:24 +0800

    hello world

---
+++ server/server.php
@@ -0,0 +1,26 @@
+<?php
+$database	= 'bmis';
+$username	= 'root';
+$host		= 'localhost';
+$password	= '24681357';
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+mysqli_report(MYSQLI_REPORT_ERROR | E_DEPRECATED | E_STRICT);
+// error_reporting(0);
+
+$conn = new mysqli($host,$username,$password,$database);
+
+if($conn->connect_error){
+	// die("Connection Failed: ". $conn->connect_error());
+	die('Connection Failed : '.$conn->connect_error);
+}
+
+if(!isset($_SESSION)){
+	session_start();	
+}
+
+if(!isset($_SESSION['username'])){
+	header('Location: login.php');
+}
+  
